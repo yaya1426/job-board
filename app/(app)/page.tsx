81878e090c7f7d662dc63a5330f61c3ed55472e8
Yahya ElAraby <yaya1426@gmail.com>
2026-03-04 00:11:29 +0200

    Day 5: Deploy Shadcn installation

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Job Board</h1>
       <p className="text-lg">Welcome to Production App!</p>
-      <Link className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600" href="/jobs">Browse Jobs</Link>
+      <Button variant="destructive">Browse Jobs</Button>
     </div>
   );
 }
